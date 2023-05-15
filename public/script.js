93863eb5309dc4c50c749ac5474a0a5cbd950c84
Wallace Felipe <wallacefelipebarros@outlook.com>
2023-05-15 18:48:49 -0300

    Commit em três páginas (produtos, produto e sobre nós)

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+/* Função para abrir e fechar a navbar no formato para mobile/tablet */
+
 const bar = document.getElementById("bar");
 const close = document.getElementById("close");
 const nav = document.getElementById("navbar");
@@ -13,3 +15,21 @@ if (close) {
     nav.classList.remove("active");
   });
 }
+
+/* Função de mudar de imagem na página de produto */
+
+var mainImg = document.getElementById("imgPrincipal");
+var smallImg = document.getElementsByClassName("imagemPequena");
+
+smallImg[0].onclick = function () {
+  mainImg.src = smallImg[0].src;
+};
+smallImg[1].onclick = function () {
+  mainImg.src = smallImg[1].src;
+};
+smallImg[2].onclick = function () {
+  mainImg.src = smallImg[2].src;
+};
+smallImg[3].onclick = function () {
+  mainImg.src = smallImg[3].src;
+};
